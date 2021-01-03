9f123f5c07a5dc55b92ee481994d8435a983b46f
herccy <15811589816@163.com>
2021-01-03 16:33:37 +0800

    修改了main：循环输出

--- main.cpp
+++ main.cpp
@@ -1,7 +1,15 @@
 #include<stdio.h>
-
+#include<iostream>
 int main() {
-	printf("hello world!!");
+	int number = 0;
+	printf("1 hello world!!\n");
+	number++;
+	int n = 3;
+	for (int i = 0; i < n; i++) {
+		std::cout << i + 2 << " " << "hello world!!" << std::endl;
+		number++;
+	}
+	std::cout << "一共打印了" << number << "个""\"hello world!!""\"" << std::endl;
 	getchar();
 	return 0;
 }
