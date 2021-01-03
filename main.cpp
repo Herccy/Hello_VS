19eda22ebdffb0112404797c4a51b728246a0ca0
herccy <15811589816@163.com>
2021-01-03 16:37:03 +0800

    修改了main

--- main.cpp
+++ main.cpp
@@ -1,15 +1,17 @@
-#include<stdio.h>
 #include<iostream>
 int main() {
 	int number = 0;
-	printf("1 hello world!!\n");
-	number++;
+	const char* s = "hello world!!";
+	std::cout << "字符串首字母是""\"" << *s << "\"" << std::endl;
+	std::cout << "字符串首字母在ASCII码中的下一个是""\"" << *s + 1 << "\"" << ",为""\"" << (char)(*s + 1) << "\"" << std::endl;
+	std::cout << "字符串第二个字母是""\"" << *(s + 1) << "\"" << std::endl;
+	std::cout << number++ << " " << s << std::endl;
 	int n = 3;
 	for (int i = 0; i < n; i++) {
-		std::cout << i + 2 << " " << "hello world!!" << std::endl;
+		std::cout << i + 2 << " " << s << std::endl;
 		number++;
 	}
-	std::cout << "一共打印了" << number << "个""\"hello world!!""\"" << std::endl;
+	std::cout << "一共打印了" << number << "个""\"" << s << "\"" << std::endl;
 	getchar();
 	return 0;
 }
