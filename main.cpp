1fbaa9cd30712b6c3375f158294910a8a974a29b
herccy <15811589816@163.com>
2021-01-11 10:01:40 +0800

    例题2.1

--- main.cpp
+++ main.cpp
@@ -1,3 +1,4 @@
+/*
 #include<iostream>
 int main() {
 	int number = 0;
@@ -14,4 +15,24 @@ int main() {
 	std::cout << "一共打印了" << number << "个""\"" << s << "\"" << std::endl;
 	getchar();
 	return 0;
+}*/
+#include <iostream>
+#include <cstdio>
+
+using namespace std;
+
+int main() {
+	// 2.1：暴力求解-枚举-例题2.1
+	//整数a,b,c 范围0~9，abc，bcc为三位数，且 abc + bcc = 532，求a，b，c
+	for (int a = 0; a <= 9; ++a) {
+		for (int b = 0; b <= 9; ++b) {
+			for (int c = 0; c <= 9; ++c) {
+				if (a * 100 + b * 110 + c * 12 == 532) {
+					printf("%d %d %d\n", a, b, c);
+				}
+			}
+		}
+	}
+	return 0;
 }
+
